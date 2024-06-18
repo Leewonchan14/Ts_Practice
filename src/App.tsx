83cccf900db7d95b338bfc97531b6faa6bba11d8
Won Chan Lee <twoone14@tukorea.ac.kr>
2024-06-18 11:28:52 +0900

    feat: Hello Component props 받기

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,11 @@
+import Hello from "./Hello.tsx";
+
 function App() {
-  const number = document.getElementById("number");
-  const increaseButton = document.getElementById("increase");
-  const decreaseButton = document.getElementById("decrease");
-  increaseButton!.onclick = () => {
-    const current = parseInt(number!.innerText, 10);
-    number!.innerText = "" + (current + 1);
-  };
-
-  decreaseButton!.onclick = () => {
-    const current = parseInt(number!.innerText, 10);
-    number!.innerText = "" + (current - 1);
-  };
-
   return (
     <>
-      <h2 id={"number"}>0</h2>
-      <div>
-        <button id={"increase"}>+1</button>
-        <button id={"decrease"}>-1</button>
-      </div>
+      <Hello name={"이원찬"} color={"red"} />
+      <Hello name={"김원찬"} color={"blue"} />
+      <Hello color={"black"} />
     </>
   );
 }
--- src/Hello.tsx
+++ src/Hello.tsx
@@ -1,5 +1,14 @@
-const Hello = () => {
-  return <div>Hello</div>;
+interface HelloProps {
+  name?: string;
+  color?: string;
+}
+
+const Hello = ({ name, color }: HelloProps) => {
+  return <div style={{ color: color }}>{name}</div>;
+};
+
+Hello.defaultProps = {
+  name: "이름없음",
 };
 
 export default Hello;
